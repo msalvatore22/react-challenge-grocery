9789f24d697f37169982e1c67bbe4f12da795f70
Michael Salvatore <msalvatore22@gmail.com>
2023-04-05 15:51:19 -0400

    Built the basic grocery line structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { useState } from 'react';
+
+const Shopper = (props) => {
+  const {items} = props
+  console.log(props)
+  return (
+    <div className="shopper">{items}</div>
+  )
+}
 
 function App() {
+  const [groceryLine, setGroceryLine] = useState([
+    [],
+    [10, 2],
+    [4, 12],
+    [],
+    [8, 9, 2],
+    [24]
+  ])
+
   return (
     <div className="App">
-      <h1>hi</h1>
+      {groceryLine.map((que, idx) => {
+        return (
+          <div className="grocery-line">
+            {que.map((items, i) => {
+              return <Shopper items={items} />
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
