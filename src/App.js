5ed2fb22cd399e3d0bed8992bfac292573bbe044
Michael Salvatore <msalvatore22@gmail.com>
2023-04-05 17:01:25 -0400

    Adding shoppers to line with least ammount of items

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState } from 'react';
 
 const Shopper = (props) => {
   const {items} = props
-  console.log(props)
   return (
     <div className="shopper">{items}</div>
   )
 }
+
 
 function App() {
   const [groceryLine, setGroceryLine] = useState([
@@ -16,11 +16,46 @@ function App() {
     [4, 12],
     [],
     [8, 9, 2],
-    [24]
+    [4]
   ])
+
+  const [checkoutValue, setCheckoutValue] = useState(0)
+
+  const handleCheckout = () => {
+    let currentSum = groceryLine[0];
+    let lineIndex = 0;
+    console.log(currentSum)
+    for(let i = 0; i < groceryLine.length; i++){
+      if(groceryLine[i].length === 0){
+        lineIndex = i
+        break
+      }
+      let sumArr = groceryLine[i].reduce((a, b) => a + b, 0)
+      
+      if(currentSum >= sumArr){
+        currentSum = sumArr
+        lineIndex = i
+      }
+    }
+    
+    let newGroceryLine = [...groceryLine]
+    newGroceryLine[lineIndex].push(checkoutValue)
+    setGroceryLine([...newGroceryLine])
+    
+  }
 
   return (
     <div className="App">
+      <div className="checkout">
+        <input
+          value={checkoutValue}
+          type="number"
+          onChange={e => setCheckoutValue(parseInt(e.currentTarget.value) ? parseInt(e.currentTarget.value) : "")}
+        ></input>
+        <button onClick={() => handleCheckout()}>Checkout</button>
+        <p>{checkoutValue}</p>
+      </div>
+      <div className="grocery-store">
       {groceryLine.map((que, idx) => {
         return (
           <div className="grocery-line">
@@ -30,6 +65,8 @@ function App() {
           </div>
         )
       })}
+      </div>
+      
     </div>
   );
 }
